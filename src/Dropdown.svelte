671f9e84cb379f9708da4746c3eefd496bc988b3
rixo <rixo@rixo.fr>
2019-12-01 09:21:01 +0100

    example: fomantic  without dynamic imports()

---
+++ src/Dropdown.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { onMount } from 'svelte'
+  import '../semantic/dist/components/transition.js'
+  import '../semantic/dist/components/dropdown.js'
+
+  onMount(() => {
+    jQuery('.ui.selection.dropdown').dropdown()
+  })
+</script>
+
+<svelte:head>
+  <link rel="stylesheet" type="text/css" href="/semantic/components/reset.css" />
+  <link rel="stylesheet" type="text/css" href="/semantic/components/transition.css" />
+  <link rel="stylesheet" type="text/css" href="/semantic/components/dropdown.css" />
+</svelte:head>
+
+<div class="ui selection dropdown">
+  <input type="hidden" name="gender" />
+  <i class="dropdown icon" />
+  <div class="default text">Gender</div>
+  <div class="menu">
+    <div class="item" data-value="1">Male</div>
+    <div class="item" data-value="0">Female</div>
+  </div>
+</div>
